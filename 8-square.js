5c8a967af502f251d67a5432525d79a4fada515a
Uchenna Hossana <uchennahossana@gmail.com>
2025-09-21 21:53:07 +0100

    Add 8-square.js ALX compliant

--- 8-square.js
+++ 8-square.js
@@ -1,10 +1,12 @@
-#!/usr/bin/node
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size)) {
+if (isNaN(size) || size < 1) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
+  let i = 0;
+  while (i < size) {
     console.log('X'.repeat(size));
+    i++;
   }
 }
+
